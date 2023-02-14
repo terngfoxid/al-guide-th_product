86bb1e319f63f1b4d503102fa07f9cb4c893364e
terngfoxid <terngfoxid@live.jp>
2023-02-14 23:41:27 +0700

    bigger text

--- components/Home_Card.tsx
+++ components/Home_Card.tsx
@@ -4,13 +4,13 @@ export default function Home_Card() {
 
     const card_style = (
         {
-            title_style: 'text-zinc-700 dark:text-zinc-200 text-2xl md:text-2xl lg:text-3xl font-bold text-center',
+            title_style: 'text-zinc-700 dark:text-zinc-200 text-2xl md:text-2xl lg:text-4xl font-bold text-center',
 
             shape: "bg-cover bg-center bg-neutral-200 w-11/12 md:w-5/6 2xl:w-full rounded-lg shadow-md border bg-neutral-200 border-gray-300 dark:border-gray-700 dark:bg-neutral-800",
             //bg-neutral-200 dark:bg-neutral-800 test-bg.jpg bg-[url('/images/test-bg.jpg')] aspect-video\
             subshape: "bg-gradient-to-b from-black/0 via-zinc-700/0 to-black/0 rounded-md ",
             position: "flex justify-center",
-            body_style: "py-2 text-zinc-600 dark:text-zinc-300 text-xs md:text-sm lg:text-lg xl:text-xl 2xl:text-2xl text-center",
+            body_style: "py-2 text-zinc-600 dark:text-zinc-300 text-xs md:text-sm lg:text-xl xl:text-2xl 2xl:text-3xl text-center",
         }
     );
 
@@ -31,11 +31,11 @@ export default function Home_Card() {
                         <div className="flex justify-center">
                             <div className="w-11/12 md:w-11/12">
                                 <div className="md:grid md:grid-cols-3">
-                                    <div className="md:col-span-2">
+                                    <div className="md:col-span-2 space-y-1">
                                         <p className="py-1 ">เว็บไซต์นี้จัดทำขึ้นเพื่อสนับสนุนผู้การเกม AzurLane ชาวไทย</p>
                                         <p className="py-1 ">โดยจะประกอบไปด้วยการแปลเนื้อหาสกิลของสาวเรือภายในเกม</p>
                                         <p className="py-1 ">คำแนะนำและไกด์ในเรื่องต่างๆอย่าง เช่น เรื่องเกียร์สวมใส่แนะนำ </p>
-                                        <p className="py-1 ">นอกจากนี้ยังมีทั้งคลิปรีวิวเรือลำต่างๆ และประวัติของเรือบางลำ</p>
+                                        <p className="py-1 ">นอกจากนี้ยังมีทั้งคลิปรีวิวเรือต่างๆ และประวัติของเรือบางลำ</p>
                                         <p className="py-1 ">เรียบเรียงมาให้ได้อ่านกันด้วยน๊าา \^-^/</p>
                                     </div>
                                     <br className="md:hidden"></br>
--- components/Panel_Card.tsx
+++ components/Panel_Card.tsx
@@ -106,7 +106,50 @@ export default function Panel_Card() {
     }
 
     if (!Array.isArray(data.data.data_update)) {
-        data_d.push(<p>ไม่สามารถโหลดข้อมูลได้</p>)
+        if (data.data.data_update.error != null) {
+            data_d.push(<p>ไม่สามารถโหลดข้อมูลได้</p>)
+        }
+        else{
+            return (
+                <div >
+                    <div className={card_style.position}>
+                        <div className={card_style.shape}>
+                            <h1 className={card_style.title_style}>
+                                <div className="md:flex md:justify-center md:items-center md:text-3xl">
+                                    <p>ประกาศ</p>
+                                </div>
+                            </h1>
+    
+                            <div className={card_style.body_style}>
+                                <div className="flex justify-center">
+                                    <ul className="flex flex-wrap -mb-px w-11/12">
+                                        <li className="mr-2">
+                                            <button id="data_update" className={mode == 0 ? card_style.list_select_style : card_style.list_style}>อัพเดตข้อมูล</button>
+                                        </li>
+                                        <li className="mr-2">
+                                            <button id="web_update" className={mode == 1 ? card_style.list_select_style : card_style.list_style}>อัพเดตเว็บ</button>
+                                        </li>
+                                        <li className="mr-2">
+                                            <button id="basic_qa" className={mode == 2 ? card_style.list_select_style : card_style.list_style}>ปัญหาการใช้งานเว็บไซต์เบื้องต้น</button>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className="block md:hidden"><div className={card_style.cut_style}></div></div>
+                                <div className="flex justify-center md:mt-0.5">
+                                    <div className={card_style.content_style + " " + card_style.mode_style[mode] + " flex justify-center mb-2 py-2"}>
+                                        <svg aria-hidden="true" className="mr-3 w-8 h-8 text-gray-400 animate-spin  fill-neutral-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                                            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                                        </svg>
+                                        Loading...
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
     }
     else {
         for (count = 0; count < data.data.data_update.length; count++) {
@@ -137,7 +180,50 @@ export default function Panel_Card() {
     }
 
     if (!Array.isArray(data.data.web_update)) {
-        data_w.push(<p>ไม่สามารถโหลดข้อมูลได้</p>)
+        if (data.data.web_update.error != null) {
+            data_w.push(<p>ไม่สามารถโหลดข้อมูลได้</p>)
+        }
+        else{
+            return (
+                <div >
+                    <div className={card_style.position}>
+                        <div className={card_style.shape}>
+                            <h1 className={card_style.title_style}>
+                                <div className="md:flex md:justify-center md:items-center md:text-3xl">
+                                    <p>ประกาศ</p>
+                                </div>
+                            </h1>
+    
+                            <div className={card_style.body_style}>
+                                <div className="flex justify-center">
+                                    <ul className="flex flex-wrap -mb-px w-11/12">
+                                        <li className="mr-2">
+                                            <button id="data_update" className={mode == 0 ? card_style.list_select_style : card_style.list_style}>อัพเดตข้อมูล</button>
+                                        </li>
+                                        <li className="mr-2">
+                                            <button id="web_update" className={mode == 1 ? card_style.list_select_style : card_style.list_style}>อัพเดตเว็บ</button>
+                                        </li>
+                                        <li className="mr-2">
+                                            <button id="basic_qa" className={mode == 2 ? card_style.list_select_style : card_style.list_style}>ปัญหาการใช้งานเว็บไซต์เบื้องต้น</button>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className="block md:hidden"><div className={card_style.cut_style}></div></div>
+                                <div className="flex justify-center md:mt-0.5">
+                                    <div className={card_style.content_style + " " + card_style.mode_style[mode] + " flex justify-center mb-2 py-2"}>
+                                        <svg aria-hidden="true" className="mr-3 w-8 h-8 text-gray-400 animate-spin  fill-neutral-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                                            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                                        </svg>
+                                        Loading...
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
     } else {
         for (count = 0; count < data.data.web_update.length; count++) {
             const buffer = count
