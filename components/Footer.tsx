72b5c52d75f19a64dc9ea2bca04ea4a85251767f
terngfoxid <terngfoxid@live.jp>
2023-02-14 14:17:20 +0700

    footer add delete data

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,13 +6,13 @@ export default function Footer() {
 
     useEffect(() => {
         document.body.classList.add("overflow-x-hidden");
-        if (localStorage.getItem('Mode') == "dark"){
+        if (localStorage.getItem('Mode') == "dark") {
             document.body.classList.add("dark");
             document.body.classList.remove("bg-neutral-100");
             document.body.classList.add("bg-neutral-900");
             setDark(true)
         }
-        else if(localStorage.getItem('Mode') == "light"){
+        else if (localStorage.getItem('Mode') == "light") {
             document.body.classList.remove("dark");
             document.body.classList.remove("bg-neutral-900");
             document.body.classList.add("bg-neutral-100");
@@ -36,9 +36,11 @@ export default function Footer() {
             creator: "Made by SSTfoxide",
             //------
             bar_style: "bg-neutral-300 shadow-xl dark:bg-neutral-800",//Footer Bar color
-            mode_style: "cursor-pointer text-xs text-zinc-600 font-bold text-center my-1 dark:text-zinc-400 w-max",
+            //mode_style: "cursor-pointer text-xs text-zinc-600 font-bold text-center my-1 dark:text-zinc-400 w-max",
             appname_style: "text-lg text-zinc-600 font-bold text-center my-1 dark:text-zinc-400",//App name style
             creator_style: "text-xs text-zinc-600 text-center my-1 italic dark:text-zinc-400",//App name style
+            ///------
+            button_style: "mx-2 rounded bg-neutral-300 hover:bg-neutral-400 dark:bg-neutral-800 dark:hover:bg-neutral-600 border border-transparent hover:border-gray-400 dark:hover:border-gray-600 text-zinc-600 dark:text-zinc-400 py-1 px-1 duration-300 hover:scale-110 text-xs font-bold text-center",
         }
     );
 
@@ -59,15 +61,33 @@ export default function Footer() {
         }
     }
 
+    const cleardata = () => {
+        if (localStorage.getItem('Mode') == "dark") {
+            localStorage.clear()
+            localStorage.setItem("Mode", "dark")
+        }
+        else {
+            localStorage.clear()
+            localStorage.setItem("Mode", "light")
+        }
+        window.location.reload();
+    }
+
     return (
         <div>
             <footer className={footer_style.bar_style}>
                 <div className='w-auto h-auto py-1'>
                     <div>
                         <div className="flex justify-center">
-                            {dark ? <p className={footer_style.mode_style} onClick={clickBTTmode}> Change Mode To Light</p>
-                                : <p className={footer_style.mode_style} onClick={clickBTTmode}> Change Mode To Dark</p>
+                            {dark ? <button className={footer_style.button_style} onClick={clickBTTmode}> Change Mode To Light</button>
+                                : <button className={footer_style.button_style} onClick={clickBTTmode}> Change Mode To Dark</button>
                             }
+
+                            <button className={footer_style.button_style}
+                                onClick={cleardata}>
+                                &nbsp;ลบข้อมูลเว็บไซต์&nbsp;
+                            </button>
+
                         </div>
                         <p className={footer_style.appname_style}>{footer_style.appname}</p>
                         <p className={footer_style.creator_style}>{footer_style.creator}</p>
