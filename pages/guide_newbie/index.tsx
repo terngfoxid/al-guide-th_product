3d26578a3945098e6ac1cc9fdb2721962c15a9cb
terngfoxid <terngfoxid@live.jp>
2023-03-05 19:24:40 +0700

    feat(guide_newbie): Create new button ,new page for Newbie and link Meaw button to youtube

---
+++ pages/guide_newbie/index.tsx
@@ -0,0 +1,48 @@
+import Head from 'next/head'
+import Topbar from '../../components/Topbar'
+import Footer from '../../components/Footer'
+import Link from "next/link";
+
+export default function Home() {
+
+    return (
+        <>
+            <Head>
+                <title>บทเรียน AL GuideTH</title>
+                <meta name="description" content="Webapp for azur lane TH comunity" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+
+            </Head>
+            <Topbar />
+
+            <main className='flex justify-center'>
+                <div className='w-full 2xl:max-w-7xl'>
+                    <br></br>
+                    <div className='flex justify-center'>
+                        <div
+                            className='w-11/12 md:w-5/6 2xl:w-full px-5 py-2 bg-neutral-200 dark:bg-neutral-800 border border-gray-300 dark:border-gray-700 rounded-lg shadow-md'
+                        >
+                            <p className='text-zinc-600 dark:text-zinc-300 text-2xl md:text-2xl lg:text-3xl font-bold text-center p-2'>บทเรียนมือใหม่</p>
+                            <div className='py-2 grid grid-cols-1 md:grid-cols-2 gap-10'>
+
+                                <Link
+                                    className="cursor-pointer w-full h-full p-3 rounded bg-neutral-300 dark:bg-neutral-700 border border-gray-300 dark:border-gray-700 hover:bg-neutral-400 dark:hover:bg-neutral-600 items-center duration-300 hover:scale-110 text-zinc-700 dark:text-zinc-200 text-base md:text-lg lg:text-xl text-center"
+                                    href="/guide_newbie/glossary"
+                                >บทเรียนที่ 1 คำศัพท์/คำย่อ
+                                </Link>
+
+                                <button
+                                    className="w-full h-full p-3 rounded bg-neutral-300 dark:bg-neutral-700 border border-gray-300 dark:border-gray-700 hover:bg-neutral-400 dark:hover:bg-neutral-600 items-center duration-300 hover:scale-110 text-zinc-700 dark:text-zinc-200 text-base md:text-lg lg:text-xl text-center"
+                                >บทเรียนที่ 2 (ไม่พร้อมใช้งาน)
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <br></br>
+                </div>
+            </main>
+            <Footer />
+        </>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,7 @@ import B_Indev_Card from '../components/button/B_Indev_Card'
 import B_Event_Card from '../components/button/B_Event_Card'
 import B_Meaw_Card from '../components/button/B_Meaw_Card'
 import B_Event_2_Card from '../components/button/B_Event_2_Card'
+import ButtonGuideNewbie from '../components/button/ButtonGuideNewbie'
 
 export default function Home() {
   return (
@@ -54,11 +55,28 @@ export default function Home() {
             <div className='w-full md:w-5/6 2xl:w-full'>
               <div className='md:grid md:grid-cols-2'>
                 <div className='flex justify-center md:justify-start'>
-                  <B_Meaw_Card/>
+                  <ButtonGuideNewbie />
                 </div>
                 <br className='block md:hidden'></br>
                 <div className='flex justify-center md:justify-end'>
                   <B_Event_2_Card />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <br className='hidden md:block'></br>
+          <br></br>
+
+          <div className='flex justify-center'>
+            <div className='w-full md:w-5/6 2xl:w-full'>
+              <div className='md:grid md:grid-cols-2'>
+                <div className='flex justify-center md:justify-start'>
+                  <B_Meaw_Card/>
+                </div>
+                <br className='block md:hidden'></br>
+                <div className='flex justify-center md:justify-end'>
+                  
                 </div>
               </div>
             </div>
