976c7a73911bd942ab55b27a8e328d27b06c145a
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2023-03-12 15:24:09 +0700

    feat: touch to change slide

--- components/Slide.tsx
+++ components/Slide.tsx
@@ -1,34 +1,61 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 export const Slide: React.FC<{
   children: JSX.Element | JSX.Element[];
   style?: React.CSSProperties;
   className?: string;
 }> = function ({ children, style, className }) {
+  const element = useRef<HTMLDivElement>(null);
   const [slideIndex, setSlideIndex] = useState<number>(0);
+  const [prevSlideIndex, setPrevSlideIndex] = useState<number>(0);
+  const [touchX, setTouchX] = useState<number>(0);
 
   const slides = Array.isArray(children) ? children : [children];
 
   return (
     <div className="relative overflow-hidden rounded-lg shadow-xl bg-neutral-200 dark:bg-neutral-900">
       <div
-        className={`flex transition duration-500 ease-in-out flex-nowrap`}
+        className="flex transition duration-300 ease-in-out flex-nowrap"
         style={{ transform: `translateX(-${slideIndex * 100}%)` }}
+        ref={element}
       >
         {slides.map((item, idx) => (
           <div
             key={idx}
             className={`flex-shrink-0 w-full h-full ${className}`}
             style={{ ...style }}
+            onTouchStart={(e) => {
+              element.current?.classList.toggle("transition");
+              element.current?.classList.toggle("duration-300");
+              setTouchX(e.touches[0].clientX);
+              setPrevSlideIndex(slideIndex);
+            }}
+            onTouchMove={(e) => {
+              const deltaX = touchX - e.touches[0].clientX;
+              const percentScreen = (deltaX / window.innerWidth) * 1.5;
+
+              if (
+                prevSlideIndex + percentScreen > 0 &&
+                prevSlideIndex + percentScreen < slides.length - 1
+              ) {
+                setSlideIndex(prevSlideIndex + percentScreen);
+              }
+            }}
+            onTouchEnd={() => {
+              element.current?.classList.toggle("transition");
+              element.current?.classList.toggle("duration-300");
+
+              setSlideIndex((current) => Math.round(current));
+            }}
           >
             {item}
           </div>
         ))}
       </div>
       <div className="absolute left-0 right-0 text-center bottom-2">
-        <span className="px-4 rounded-full text-zinc-600 dark:text-zinc-400 bg-neutral-200 dark:bg-neutral-900 bg-opacity-80 dark:bg-opacity-80">{`${
-          slideIndex + 1
-        } / ${slides.length}`}</span>
+        <span className="px-4 rounded-full text-zinc-600 dark:text-zinc-400 bg-neutral-200 dark:bg-neutral-900 bg-opacity-80 dark:bg-opacity-80">{`${Math.round(
+          Math.round(slideIndex + 1),
+        )} / ${slides.length}`}</span>
       </div>
       <button
         className="absolute top-0 bottom-0 left-0 p-2 transition text-zinc-600 dark:text-zinc-400 bg-neutral-200 dark:bg-neutral-900 bg-opacity-80 dark:bg-opacity-80 hover:bg-opacity-70 dark:hover:hover:bg-opacity-70"
