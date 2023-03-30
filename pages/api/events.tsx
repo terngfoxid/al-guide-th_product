53366343cc194123b5590c05605ed101062cd597
Methapon2001 <61303214+Methapon2001@users.noreply.github.com>
2023-03-30 11:26:57 +0700

    Slide active event (#7)
    * refactor: active event with slides
    
    * chore: fix cropped image and empty new ships box
    
    * chore: infer type null
    
    * feat: zoom image component
    
    * feat: zoom image, small slide image adjustment
    
    * chore: adjust loading position

---
+++ pages/api/events.tsx
@@ -0,0 +1,30 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import db from "../../utils/db";
+
+export default async function handler(
+  _req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const snapshot = await db.collection("dev_event").get();
+  const docs = snapshot.docs.map((doc) => doc.data());
+
+  res.status(200).json(
+    docs
+      .filter((item) => item._enable)
+      .sort((a, b) => b._priority - a._priority)
+      .map((item) => {
+        delete item._priority;
+        delete item._enable;
+
+        if (item._new) {
+          item.type = "new";
+        } else {
+          item.type = "rerun";
+        }
+
+        delete item._new;
+
+        return item;
+      }),
+  );
+}
