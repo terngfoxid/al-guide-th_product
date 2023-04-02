3cf4072dadc9e701493d2d5b5e37b331871c9a7b
terngfoxid <terngfoxid@live.jp>
2023-04-02 15:02:58 +0700

    clean(project): remove nextjs-google-analytics

---
+++ gtag.js
@@ -0,0 +1,8 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-L4CJGVFV5D"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-L4CJGVFV5D');
+</script>
