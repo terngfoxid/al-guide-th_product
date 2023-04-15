d9dc83514660b7ffd6645775007d946c2ff240ae
terngfoxid <terngfoxid@live.jp>
2023-04-15 11:44:37 +0700

    feat(coredata): add core data newbie guide

---
+++ pages/guide_newbie/coredata.tsx
@@ -0,0 +1,82 @@
+import Head from "next/head";
+import Topbar from "../../components/Topbar";
+import Footer from "../../components/Footer";
+import BackToTop from "../../components/overlay/BackToTop";
+
+export default function Home() {
+
+    return (
+        <>
+            <Head>
+                <title>บทเรียนที่ 6 Core Data | Azur Lane Guide TH</title>
+                <meta name="description" content="Core Data Azur Lane , Core Data หาจากใหน , Core Data ควรแลกอะไร , Core Shop Azur Lane" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Topbar />
+
+            <main className="flex justify-center">
+                <div className="w-full 2xl:max-w-7xl">
+                    <div className="grid grid-cols-1 py-5 space-y-3 md:space-y-5">
+                        <div className="flex justify-center">
+                            <div className="w-11/12 border border-gray-300 rounded-lg shadow-md md:w-5/6 2xl:w-full bg-neutral-200 dark:bg-neutral-800 dark:border-gray-700 overflow-hidden">
+                                <img
+                                    className="w-full h-full"
+                                    src="/images/learning/act 6/banner.webp"
+                                    alt="Core Data Azur Lane"
+                                ></img>
+                            </div>
+                        </div>
+                        <div className="flex justify-center">
+                            <div className="w-11/12 border border-gray-300 rounded-lg shadow-md md:w-5/6 2xl:w-full bg-neutral-200 dark:bg-neutral-800 dark:border-gray-700 overflow-hidden">
+                                <img
+                                    className="w-full h-full"
+                                    src="/images/learning/act 6/1.webp"
+                                    alt="Core Data คืออะไร"
+                                ></img>
+                            </div>
+                        </div>
+                        <div className="flex justify-center">
+                            <div className="w-11/12 border border-gray-300 rounded-lg shadow-md md:w-5/6 2xl:w-full bg-neutral-200 dark:bg-neutral-800 dark:border-gray-700 overflow-hidden">
+                                <img
+                                    className="w-full h-full"
+                                    src="/images/learning/act 6/2.webp"
+                                    alt="Core Data หาจากใหน"
+                                ></img>
+                            </div>
+                        </div>
+                        <div className="flex justify-center">
+                            <div className="w-11/12 border border-gray-300 rounded-lg shadow-md md:w-5/6 2xl:w-full bg-neutral-200 dark:bg-neutral-800 dark:border-gray-700 overflow-hidden">
+                                <img
+                                    className="w-full h-full"
+                                    src="/images/learning/act 6/3.webp"
+                                    alt="Core Data แลกอะไรดี"
+                                ></img>
+                            </div>
+                        </div>
+                        <div className="flex justify-center">
+                            <div className="w-11/12 border border-gray-300 rounded-lg shadow-md md:w-5/6 2xl:w-full bg-neutral-200 dark:bg-neutral-800 dark:border-gray-700 overflow-hidden">
+                                <img
+                                    className="w-full h-full"
+                                    src="/images/learning/act 6/4.webp"
+                                    alt="Core Data แลกอะไรดี"
+                                ></img>
+                            </div>
+                        </div>
+                        <div className="flex justify-center">
+                            <div className="w-11/12 border border-gray-300 rounded-lg shadow-md md:w-5/6 2xl:w-full bg-neutral-200 dark:bg-neutral-800 dark:border-gray-700 overflow-hidden">
+                                <img
+                                    className="w-full h-full"
+                                    src="/images/learning/act 6/5.webp"
+                                    alt="Core Data แลกอะไรดี"
+                                ></img>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+            <BackToTop />
+            <Footer />
+        </>
+    );
+}
