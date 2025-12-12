6de2d74cfcee20fdf85c447c7e13668c42378995
itisit terngfoxid <terngfoxid@live.jp>
2025-12-13 03:38:32 +0700

    [fix]landing page

--- components/secretary.tsx
+++ components/secretary.tsx
@@ -10,8 +10,8 @@ export default function Secretary() {
     }, []);
 
     return (
-        <div className="fixed top-1/2 left-[70%] lg:left-1/2 -translate-x-1/4 lg:-translate-x-1/2 -translate-y-1/2 min-w-[120vw] lg:min-w-[60vw] w-[70vw] lg:max-w-[80vw] xl:max-w-[60vw] md:max-h-[80vh]">
-            <img alt="Secretary" src={secretaryUrl? secretaryUrl:"/images/secretary.png"} className="h-full max-h-[100vh]" />
+        <div className="fixed top-1/2 left-[70%] lg:left-1/2 -translate-x-1/4 lg:-translate-x-1/2 -translate-y-1/2 min-w-[120vw] lg:min-w-[60vw]">
+            <img alt="Secretary" src={secretaryUrl? secretaryUrl:"/images/secretary.png"} className="max-h-[100vh]" />
         </div>
     );
 }
